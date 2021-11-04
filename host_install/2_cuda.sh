a6b58cadd20f30127b540a1403481e9dc67a3056
Hoosang Lee <lhs223@gmail.com>
2021-11-04 21:53:44 +0900

    update

--- host_install/2_cuda.sh
+++ host_install/2_cuda.sh
@@ -3,7 +3,7 @@ sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
 sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/7fa2af80.pub
 sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update
-sudo apt-get -y install cuda
+sudo apt-get -y install cuda-11.4
 
 #echo "export PATH=/usr/local/cuda-11.4/bin:\"$PATH\"" >> ~/.bashrc
 #echo "export LD_LIBRARY_PATH=/usr/local/cuda-11.4/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
