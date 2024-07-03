966db353a22b94bc5cd2d17d02727cfd15c4d102
Hoosang Lee <lhs223@gmail.com>
2024-07-03 10:28:40 +0900

    update gitignores

--- docker/docker_containers/ur10_container/share/catkin_ws/build/Universal_Robots_ROS_Driver/ur_robot_driver/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/share/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver
-# Build directory: /root/share/catkin_ws/build/Universal_Robots_ROS_Driver/ur_robot_driver
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(_ctest_ur_robot_driver_rostest_test_driver.test "/root/share/catkin_ws/build/catkin_generated/env_cached.sh" "/usr/bin/python2" "/opt/ros/melodic/share/catkin/cmake/test/run_tests.py" "/root/share/catkin_ws/build/test_results/ur_robot_driver/rostest-test_driver.xml" "--return-code" "/usr/bin/python2 /opt/ros/melodic/share/rostest/cmake/../../../bin/rostest --pkgdir=/root/share/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver --package=ur_robot_driver --results-filename test_driver.xml --results-base-dir \"/root/share/catkin_ws/build/test_results\" /root/share/catkin_ws/src/Universal_Robots_ROS_Driver/ur_robot_driver/test/driver.test ")
